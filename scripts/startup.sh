9f81ddf17d8f272773a737abf7f4259dde4a7bad
Joe Khoury <jfkhoury@gmail.com>
2020-02-29 08:52:24 -0500

    Fix script to open Chrome after startup.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
 
-# Remove the directory if already there to get a fresh copy
 if [ -d ~/Desktop/Summit2020AlloyLab ]
 then
-    echo "Folder Already Exists"
     rm -Rf ~/Desktop/Summit2020AlloyLab
 fi
 
-# Pull the most recent version of the Lab File
-echo "Download Lab Assets"
+function print {
+    msg=$1
+    echo "\n----------------------"
+    echo ${msg}
+    echo "----------------------\n"
+}
+
+function openBrowser {
+    print "Opening Chrome Tabs..."
+    open -a Google\ Chrome "http://launch.adobe.com/"
+    open -a Google\ Chrome "http://alloyio.com:8080"
+}
+
+print "Cloning the lab's repo..."
 cd ~/Desktop
 git clone --recurse-submodules https://github.com/Adobe-Marketing-Cloud-Activation/Summit2020AlloyLab.git Summit2020AlloyLab
 cd Summit2020AlloyLab
 git fetch --all
 git pull 
-npm i
-npm run start
-# cd site
-# git submodule init
-# git submodule update
 
-# Launch the localhost pages
-echo "Open Chrome Tabs"
-open -a Google\ Chrome "http://launch.adobe.com/"
-open -a Google\ Chrome "http://alloyio.com:8080"
-# open -a Google\ Chrome "http://bit.ly/Lab779"
+print "Installing dependencies..."
+npm i
+
+print "Starting the lab's website..."
+npm run start | openBrowser
 
 exit
